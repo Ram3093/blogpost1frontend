6f98ca7682d720b1d99a31bc00c89eb55395c255
Ram kumar <rk16013093@gmail.com>
2020-12-18 00:53:04 +0530

    about blogstore functionally

--- src/components/body-layout/About.js
+++ src/components/body-layout/About.js
@@ -7,18 +7,11 @@ import React, { Component } from 'react'
                 <div>
                     <h5>A blog website</h5>
                     <p>
-                    There are many variations of passages of Lorem Ipsum available,
-                     but the majority have suffered alteration in some form,
-                      by injected humour, or randomised words which don't look 
-                      even slightly believable. If you are going to use a passage 
-                      of Lorem Ipsum, you need to be sure there isn't anything 
-                      embarrassing hidden in the middle of text. All the Lorem 
-                      Ipsum generators on the Internet tend to repeat predefined 
-                      chunks as necessary, making this the first true generator 
-                      on the Internet. It uses a dictionary of over 200 Latin words,
-                       combined with a handful of model sentence structures,
-                        to generate Lorem Ipsum which looks reasonable.
+                     For writing blogs in our website you need first to register/login.
                     </p>
+                    <p>On signup you will get a unique batch with name.</p>
+                    <p>After signup you will be able to write blog in our website with you cool batch.</p>
+                    
                 </div>
             </div>
         )
